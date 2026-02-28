a9af4ed03c8419614ac9520d9257831e2cee33f8
Trevor King <tlking447@gmail.com>
2026-02-27 21:06:56 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if not api_key:
+    RuntimeError("Key not found")
+client = genai.Client(api_key=api_key)
+
+response = client.models.generate_content(model='gemini-2.5-flash', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+print(f"Response: {response.text}")
